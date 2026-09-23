e44b8bb1b9aeb4c28a6dbfaf83500c37c52d5b66
Gui6456 <guiborgia@gmail.com>
2026-09-23 15:18:58 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './css/index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
 import ObrasMangas from './ObrasMangas.jsx'
 import Historico from './Historico.jsx'
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
             <Route path="/" element={<App />} />
             <Route path="/ObrasMangas" element={<ObrasMangas />} />
             <Route path="/Historico" element={<Historico />} />
+           
         </Routes>
     </BrowserRouter>
     
