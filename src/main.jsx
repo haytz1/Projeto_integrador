25c58118fa4d6269003d7f66acee5f1b11507d51
Gui6456 <guiborgia@gmail.com>
2026-09-23 14:09:36 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    
   </StrictMode>,
 )
